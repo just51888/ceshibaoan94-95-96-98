f5838271c6f80ef1e306527257f9962426e34e98
just51888 <zwiyoarindea@hotmail.com>
2026-07-03 01:28:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # ======================== 配置区 ========================
-TOKEN = os.environ.get("8221274039:AAG2WMUj9EqsGsX-KMIQAFaY2SR1ARtMUIc")
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 ADMIN_ID = 7140260550  # 统一管理员 ID
 
 name_map = {
